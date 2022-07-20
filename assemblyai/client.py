c86cd27bea9a9b116af5951481649dd7b43cae15
jeadie <jackeadie@duck.com>
2022-07-20 17:04:29 +1000

    add basic request to Client

--- assemblyai/client.py
+++ assemblyai/client.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Optional
+
 import httpx
 
 from assemblyai.api_endpoints import StreamEndpoint, TranscriptEndpoint, UploadEndpoint
@@ -20,3 +22,26 @@ class Client:
         self.upload = UploadEndpoint(self)
         self.stream = StreamEndpoint(self)
         
+
+    def _build_request(self, method: str, path: str, query: Optional[Dict[Any, Any]] = None, body: Optional[Dict[Any, Any]] = None) -> httpx.Request:
+        """Build a request object for the client."""
+        return self.client.build_request(
+            method, path, params=query, json=body,
+        )
+
+    def _parse_response(self, response: httpx.Response) -> Any:
+        """Parses the response from a client request. Throws a httpx.HTTPStatusError if an error status code is returned."""
+        response.raise_for_status()
+        return response.json()
+
+    
+    def request(self, path: str, method: str, query: Optional[Dict[Any, Any]] = None, body: Optional[Dict[Any, Any]] = None) -> Any:
+        """ Sends a JSON-encoded, HTTP request with required authorization.
+        
+        Throws:
+            httpx.HTTPStatusError: If the response was unsuccessful.
+            httpx.TimeoutException: If a timeout occured on the request.
+        """
+        request = self._build_request(method, path, query, body)
+        response = self.client.send(request)
+        return self._parse_response(response)
